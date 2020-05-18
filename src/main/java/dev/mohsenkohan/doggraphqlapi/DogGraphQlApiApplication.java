298c63ae0a8f894a2e1e0fb5b9e2cb239394972c
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-05-18 15:04:59 +0430

    Initial project commit

---
+++ src/main/java/dev/mohsenkohan/doggraphqlapi/DogGraphQlApiApplication.java
@@ -0,0 +1,13 @@
+package dev.mohsenkohan.doggraphqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogGraphQlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogGraphQlApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mohsenkohan/doggraphqlapi/DogGraphQlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mohsenkohan.doggraphqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogGraphQlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
